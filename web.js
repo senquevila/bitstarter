4ac58ecce61e44286780575b8c1b4b9c13cb934e
Enrique <Enrique@Enrique-Des.invilp.com>
2013-07-19 12:23:08 -0600

    Modificacion de web.js para leer contenido de index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var myBuffer = new Buffer(fs.readFileSync('index.html', 'utf8'));
+  
+  response.send(myBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
