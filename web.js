d8dcecd019d9d72e17bf154c4df6597055407631
senquevila <senquevila@gmail.com>
2013-09-05 16:29:21 -0600

    La carga del contenido dinamico paso a web.js

--- web.js
+++ web.js
@@ -5,8 +5,16 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var myBuffer = new Buffer(fs.readFileSync('index.html', 'utf8'));
+  var myBufferStr = myBuffer.toString();
+  var backers = 1;
+  var amount = 1.22;
+  var day_left = 60;
+
+  myBufferStr = myBufferStr.replace("{__backers}", backers, "g");
+  myBufferStr = myBufferStr.replace("{__amount}", amount, "g");
+  myBufferStr = myBufferStr.replace("{__day_left}", day_left, "g");
   
-  response.send(myBuffer.toString());
+  response.send(myBufferStr);
 });
 
 var port = process.env.PORT || 8080;
